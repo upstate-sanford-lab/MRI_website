ed964745c94094724c6fc29b634b6c4e5da65239
upstate-sanford-lab <59257255+upstate-sanford-lab@users.noreply.github.com>
2020-05-28 18:37:23 -0400

    Update app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,13 +1,15 @@
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 import pydicom as dicom
 import numpy as np
 import os
-import cv2
+from PIL import Image
+
 import natsort
 import subprocess
 '''
+import cv2
 from werkzeug.datastructures import ImmutableMultiDict
 import json
 import matplotlib.pyplot as plt
@@ -99,7 +101,7 @@ def gather_files(path, type, wholeDIR):
                     List.append(os.path.join(dirName,filename))
     return List
 
-def makeJPGs(DCM_location, JPG_location, windowing):
+def makeJPGs(DCM_location, JPG_location, series):
     '''Takes a folder DCM_location, converts dcm into jpg, and saves files in JPG_location'''
     saved_file_list = []
     for s in os.listdir(DCM_location):
@@ -108,26 +110,24 @@ def makeJPGs(DCM_location, JPG_location, windowing):
             if ".dcm" in s.lower():  # check whether the file is DICOM
                 ds = dicom.dcmread(os.path.join(DCM_location, s))
                 ps = ds.pixel_array.astype(float)
-                if windowing == True:
-                    # for highb series
-                    pixels = ((ps - ps.min()) * (1/(ps.max() - ps.min()) * 255))
-                    # apply mask and rescale array to 0-255
-                    #masked_image_wp = math_img("img1 * img2", img1=nii_img, img2=wp_mask)
-                    #nii_rescaled = math_img("((img1 - img1.min()) * (1/(img1.max() - img1.min()) * 255))", img1=masked_image_wp)
-                else:
-                    if ps.max() == 0:
-                        pixels = (np.maximum(ps, 0)) * 255.0
-                    else:
-                        pixels = (np.maximum(ps, 0) / ps.max()) * 255.0
+                # if series == "highb":
+                pixels = ((ps - ps.min()) / (ps.max() - ps.min()))* 255.0
+                # elif series == "t2" or series == "adc":
+                #     if ps.max() == 0:
+                #         pixels = (np.maximum(ps, 0)) * 255.0
+                #     else:
+                #         pixels = (np.maximum(ps, 0) / ps.max()) * 255.0
                 pixels = np.uint8(pixels)
-                s = s.replace('.dcm', '.jpg')
+                im = Image.fromarray(pixels)
+                im = im.convert("L")
 
+                s = s.replace('.dcm', '.jpg')
                 if hasattr(ds, "SliceLocation"):
                     s = str(round(ds.SliceLocation, 2)) + ", " + s
                 else:
-                    'Slice was either aligned already or it was a scout view'
+                    '''Slice was either aligned already or it was a scout view'''
 
-                cv2.imwrite(os.path.join(JPG_location, s), pixels)
+                im.save(os.path.join(JPG_location, s))
                 saved_file_list.append(os.path.join("static","JPG_converts", s))
         except:
             print("Could not convert: " + s)
@@ -141,12 +141,8 @@ def allowed_file(filename):
 
 def SaveSeries(file, filename, series):
     '''This function saves a "file" under a specific "filename" in one of three folders specified by "series" (adc, highb, t2)'''
-    if series == "adc":
-        file.save(os.path.join(app.config['adc_uploads'], filename))
-    elif series == "highb":
-        file.save(os.path.join(app.config['highb_uploads'], filename))
-    elif series == "t2":
-        file.save(os.path.join(app.config['t2_uploads'], filename))
+    if series in ["adc", "highb", "t2"]:
+        file.save(os.path.join(app.config[series + '_uploads'], filename))
     else:
         return False
     return True
@@ -176,9 +172,20 @@ def cleanup(images, append, isolateFile):
         count = count + 1
     return [images,count]
 
+def checkLogin(name, password):
+    users = {"Tom": "prostate", "Andrew": "webdev"}
+    if users[name] == password:
+        return True
+    else:
+        return False
+
+
+
 app = Flask(__name__)
 '''Usage: update basepath with the location of the WebApp folder'''
 basePath = r'C:\Users\MSStore'
+loggedIn = False
+user = "no one"
 
 for dir in ["uploads", "JPG_converts", "Aligned_DICOM", "jpg_tumor"]:
     app.config[dir] = os.path.join(basePath, "WebApp", "static", dir)
@@ -187,55 +194,68 @@ for dir in ["uploads", "JPG_converts", "Aligned_DICOM", "jpg_tumor"]:
 
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'dcm'}
-
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 @app.route('/Homepage', methods = ["GET", "POST"])
-def index ():
-    '''This is the function for the Homepage of the website.
-    "Post" requests occur when a request is sent to upload dicoms to the server'''
-    clearFiles(app.config["Aligned_DICOM"])
-    clearFiles(app.config["JPG_converts"])
-    clearFiles(app.config["uploads"])
-    clearFiles(app.config["jpg_tumor"])
+def index():
+    global loggedIn
+    if loggedIn == False:
+        return redirect(url_for("login"))
+        print("redirected to login from homepage")
+        print(loggedIn)
+    else:
+        for folder in ["Aligned_DICOM", "JPG_converts", "uploads", "jpg_tumor"]:
+            clearFiles(app.config[folder])
 
-    uploaded = 0
-    FileTypes = ["adc", "highb", "t2"]
-    if request.method == 'POST':
-        print(request.files)
-        for filetype in FileTypes:
-            for file in request.files.getlist(filetype):
-                successfulSave = False
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    successfulSave = SaveSeries(file,filename, filetype)
-                elif not allowed_file(file.filename):
-                    print(file.filename)
-                    print("Not allowed file type")
-                else:
-                    print(file.filename)
-                    print("No file part")
-                if successfulSave == True:
-                    uploaded = uploaded + 1
-        if uploaded >0:
-            subprocess.call(["Python", "Alignment.py", app.config["t2_uploads"], app.config["adc_uploads"], app.config["highb_uploads"], app.config["Aligned_DICOM"]])
-            image_list = makeJPGs(app.config["adc_Aligned_DICOM"], app.config["adc_JPG_converts"], False)
-            image_list.append(makeJPGs(app.config["highb_Aligned_DICOM"], app.config["highb_JPG_converts"], True))
-            image_list.append(makeJPGs(app.config["t2_Aligned_DICOM"], app.config["t2_JPG_converts"], False))
-            print("the following images have been saved as jpgs")
-            print(image_list)
-        else:
-            print("no files were uploaded")
+        uploaded = 0
+        if request.method == 'POST':
+            print(request.files)
+            for filetype in ["adc", "highb", "t2"]:
+                for file in request.files.getlist(filetype):
+                    successfulSave = False
+                    if file and allowed_file(file.filename):
+                        filename = secure_filename(file.filename)
+                        successfulSave = SaveSeries(file,filename,filetype)
+                    elif not allowed_file(file.filename):
+                        print(file.filename)
+                        print("Not allowed file type")
+                    else:
+                        print(file.filename)
+                        print("No file part")
+                    if successfulSave == True:
+                        uploaded = uploaded + 1
+            if uploaded >0:
+                imagelist = [];
+                subprocess.call(["Python", "Alignment.py", app.config["t2_uploads"], app.config["adc_uploads"], app.config["highb_uploads"], app.config["Aligned_DICOM"]])
+                for series in ["adc", "highb", "t2"]:
+                    imagelist.append(makeJPGs(app.config[series + "_Aligned_DICOM"], app.config[series+"_JPG_converts"], series))
+                print("the following images have been saved as jpgs")
+                print(imagelist)
+            else:
+                print("no files were uploaded")
+        global user
+        return render_template("PIRADS_webAI.html", username = user)
 
-    return render_template("PIRADS_webAI.html")
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     '''When the webpage needs '''
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    '''When the webpage needs '''
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+# @app.route('/Viewer')
+# def Viewer ():
+#     return render_template('PIRADS_webAI.html')
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        print("attempt for login")
+        if checkLogin(request.form["username"], request.form["password"]):
+            global user
+            global loggedIn
+            loggedIn = True
+            user = request.form["username"]
+            return redirect(url_for("index"))
 
-@app.route('/Viewer')
-def Viewer ():
-    return render_template('PIRADS_webAI.html')
+    return render_template("login.html")
+
 
 @app.route('/api/images')
 def api_get_images():
